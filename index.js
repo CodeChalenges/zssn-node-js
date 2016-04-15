6b49be505aecbba5fe984593a3ac978ce90c9576
Mauricio Klein <cod_mklein@uolinc.com>
2016-04-15 13:34:31 -0300

    Base project setup created.

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+// Mapping routes
+var routes = require('./lib/routes');
+routes(app);
+
+var server = app.listen(8081, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Zombie Survival Social Network API listening at http://%s:%s", host, port);
+});
